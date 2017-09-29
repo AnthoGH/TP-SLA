2bf3ae3702ed568271fd896dbb5394c684b316e5
AnthoGH <anthogeorg69@gmail.com>
2017-09-29 10:36:45 +0200

    Update newtable.sql

--- SQLGSB/newtable.sql
+++ SQLGSB/newtable.sql
@@ -1,3 +1,15 @@
+CREATE TABLE `Visiteur` (
+  `id` char(4) NOT NULL,
+  `nom` char(30) DEFAULT NULL,
+  `prenom` char(30) DEFAULT NULL,
+  `login` char(20) DEFAULT NULL,
+  `mdp` char(20) DEFAULT NULL,
+  `adresse` char(30) DEFAULT NULL,
+  `cp` char(5) DEFAULT NULL,
+  `ville` char(30) DEFAULT NULL,
+  `dateEmbauche` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `Absence` (
   `refVisiteur` char(4) NOT NULL,
   `date` date NOT NULL,
